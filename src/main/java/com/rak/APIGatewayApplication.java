d89ab02db89a4d8f876b50290952f4c0d3567273
hamza ahmad <hamza@digitify.com>
2024-01-31 14:54:26 +0500

    Initial commit

---
+++ src/main/java/com/rak/APIGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.rak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class APIGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(APIGatewayApplication.class, args);
+	}
+
+}
